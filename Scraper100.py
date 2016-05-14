c3609c1d2402a89d74cf204d22cb3f4658f4d673
Deb K Roberts <deborah@deborahroberts.info>
2016-05-14 17:44:04 +0100

    Add files via upload

---
+++ Scraper100.py
@@ -0,0 +1,17 @@
+# Scraper 1.0.0
+# AntyZebra 13 May 2016
+# Simple URL scraper (snipped based on html <body> tag and saved to file)
+
+### IMPROVEMENT: USER-INPUT URL
+
+import urllib
+fout = open('textfilename.txt', 'w')
+fhand1 = urllib.urlopen('http://deborahroberts.info/test-text/')
+text1 = fhand1.read()
+pos = text1.find('<body')
+pos1 = text1.find('</body>')
+snipped = text1[pos:pos1+7]
+print snipped
+
+fout.write(snipped)
+fout.close()
